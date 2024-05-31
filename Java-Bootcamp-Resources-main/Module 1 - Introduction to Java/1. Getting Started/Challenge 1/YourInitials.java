bddc2603f32bf43388f090ec42a3ac39cb1c02e2
Eric Brose <ericbrose@gmail.com>
2024-05-30 22:41:24 -0700

    day 1

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("EEEEE  BBBB"); 
+        System.out.println("E      B   B"); 
+        System.out.println("E      B   B"); 
+        System.out.println("EEE    BBBB"); 
+        System.out.println("E      B   B"); 
+        System.out.println("E      B   B"); 
+        System.out.println("EEEEE  BBBB"); 
+    }
+}
---
+++ section_01/HelloJava.java
@@ -0,0 +1,11 @@
+package section_01;
+/**
+ * HelloJava
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Greetings Java");
+    }
+
+}
+
