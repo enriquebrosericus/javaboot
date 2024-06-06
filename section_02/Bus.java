5345450ee9ef871313cc86a814c11ff8f4da660f
Eric Brose <edawg@Erics-MacBook-Air.local>
2024-06-06 07:05:43 -0700

    course fun

---
+++ section_02/Bus.java
@@ -0,0 +1,13 @@
+package section_02;
+
+public class Bus {
+   public static void main(String[] args) {
+      int passengers = 0;
+      passengers = passengers + 9;
+      System.out.println(passengers);
+      passengers = passengers - 1;
+      System.out.println(passengers);
+
+
+}
+}
---
+++ section_02/Variables.java
@@ -0,0 +1,14 @@
+package section_02;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int passengers = 5;
+        int busTickets = passengers;
+        int maxValue = 2147483647;
+        int minValue = -2147483648;
+        System.out.println(passengers);
+        System.out.println(busTickets);
+    }
+    
+}
