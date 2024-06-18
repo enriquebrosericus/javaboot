3c73035967725a99a9faa5534357e4c1150ac7b4
Eric Brose <ericbrose@gmail.com>
2024-06-18 06:47:17 -0700

    course work

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/scratch/Grades.java
@@ -0,0 +1,16 @@
+public class Grades {
+
+    public static void main(String[] args) {
+        char tacoGrade = 'A';
+        char burritoGrade = 'B';
+        char enchiladaGrade = 'C';
+
+        System.out.println("Taco Grade: " + tacoGrade);
+        System.out.println("Burrito Grade: " + burritoGrade);
+        System.out.println("Enchilada Grade: " + enchiladaGrade);
+
+//tell the student the overall grade
+        System.out.println("Overall Grade: " + tacoGrade + burritoGrade + enchiladaGrade);
+
+    }
+}
