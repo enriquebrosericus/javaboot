d2fb2a482efaa3543255689888d7f86d2c3b1331
Eric Brose <edawg@Erics-MacBook-Air.local>
2024-09-07 07:18:31 -0700

    morning course stuff

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2 Solution/WholeNums.java
@@ -0,0 +1,12 @@
+public class WholeNums {
+    public static void main(String[] args) {
+        long globalPopulation = 8_000_000_000L;
+        System.out.println("The global population is " + globalPopulation);
+        long googleSearches = 8_500_000_000L;
+        System.out.println("The number of Google searches is " + googleSearches);
+
+
+
+
+    }
+}
